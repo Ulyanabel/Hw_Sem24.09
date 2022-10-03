d7070f280c3c7fed8254e21567563070ea85ffe2
ulyana_belousova <tentakly@list.ru>
2022-10-03 14:04:58 +0300

    task 58

--- task_58/Program.cs
+++ task_58/Program.cs
@@ -1,49 +1,41 @@
 ﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц
-void InputMatrix(int[,] matrix)
+void NewMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
-            matrix[i, j] = new Random().Next(0, 11);
-    }
-}
-
-void PrintMatrix(int[,] matrix)
-{
-    Console.WriteLine();
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-            Console.Write("{0,5}", matrix[i, j]);
+        {
+            matrix[i, j] = new Random().Next(-10, 11);
+            Console.Write(matrix[i, j] + " \t");
+        }
         Console.WriteLine();
     }
 }
 
-int[,] ReleaseMatrix(int[,] matrixA, int[,] matrixB)
+void ProzMatrix(int[,] MatrixOne, int[,] MatrixTwo)
 {
-    int[,] matrixC=new int[matrixA.GetLength(0),matrixB.GetLength(1)];
-    for (int i = 0; i < matrixC.GetLength(0); i++)
+    Console.WriteLine();
+   int[,] ResultMatrix = new int[MatrixOne.GetLength(0), MatrixOne.GetLength(1)];
+    for (int i = 0; i < MatrixOne.GetLength(0); i++)
     {
-        for (int j = 0; j < matrixC.GetLength(1); j++)
+        for (int j = 0; j < MatrixOne.GetLength(1); j++)
         {
-            for (int k=0;k<matrixB.GetLength(0);k++)
-                matrixC[i,j]=matrixC[i,j]+matrixA[i,k]*matrixB[k,j];
+            ResultMatrix[i, j] = MatrixOne[i, j] * MatrixTwo[i, j];
+            Console.Write(ResultMatrix[i, j] + " ");
         }
-    }
-return matrixC;
+         Console.WriteLine();
+    } 
 }
 
-Console.Write("Введите кол-во строк: ");
+Console.Write("Введите количество строк: ");
 int n = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите кол-во столбцов: ");
+Console.Write("Введите количество столбцов: ");
 int m = Convert.ToInt32(Console.ReadLine());
-int[,] matrixA = new int[n, m];
-int[,] matrixB = new int[m, n];
-InputMatrix(matrixA);
-Console.WriteLine("Исходная матрица А: ");
-PrintMatrix(matrixA);
-InputMatrix(matrixB);
-Console.WriteLine("Исходная матрица В: ");
-PrintMatrix(matrixB);
-Console.WriteLine("Произведение матрицы А и матрицы В: ");
-PrintMatrix(ReleaseMatrix(matrixA,matrixB));
+int[,] MatrixOne = new int[n, m];
+int[,] MatrixTwo = new int[n, m];
+Console.WriteLine("Начальная матрица 1: ");
+NewMatrix(MatrixOne);
+Console.WriteLine();
+Console.WriteLine("Начальная матрица 2: ");
+NewMatrix(MatrixTwo);
+ProzMatrix(MatrixOne, MatrixTwo);
