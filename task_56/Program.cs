555bd7626ea2e81aa66ba2bee881a41aa1305724
ulyana_belousova <tentakly@list.ru>
2022-10-03 13:41:40 +0300

    task 56

--- task_56/Program.cs
+++ task_56/Program.cs
@@ -1,42 +1,39 @@
 ﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов
-void InputMatrix(int[,] matrix)
+void NewMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
-            matrix[i, j] = new Random().Next(0, 11);
-    }
-}
-
-void PrintMatrix(int[,] matrix)
-{
-    Console.WriteLine();
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 11);
             Console.Write(matrix[i, j] + " \t");
+        }
         Console.WriteLine();
     }
 }
 
-int RealeseMatrix(int[,] matrix)
+void SortRowInMatrix(int[,] matrix)
 {
-    int MinRowSum = 0, Row = -1;
+    int[] SumRow = new int[matrix.GetLength(0)];
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        int RowSum = 0;
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            RowSum += matrix[i, j];
+            SumRow[i] += matrix[i, j];
         }
-        if (MinRowSum == 0 || MinRowSum > RowSum)
-        {
-            MinRowSum = RowSum;
-            Row = i+1;
-        }
-        Console.WriteLine($"Номер строки {i+1} Сумма: {RowSum};");
     }
-    return Row;
+    int temp = SumRow[0];
+    int NumberRow = 0;
+    for (int i = 0; i < SumRow.Length; i++)
+    {
+        if (SumRow[i] < temp)
+        {
+            temp = SumRow[i];
+            NumberRow = i + 1;
+        }
+    }
+    Console.WriteLine("Минимальная сумма элементов в строке:" + NumberRow);
+    Console.WriteLine("Минимальная сумма элементов равна:" + temp);
 }
 
 Console.Write("Введите количество строк: ");
@@ -44,7 +41,6 @@ int n = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите количество столбцов: ");
 int m = Convert.ToInt32(Console.ReadLine());
 int[,] matrix = new int[n, m];
-InputMatrix(matrix);
-PrintMatrix(matrix);
-Console.WriteLine();
-Console.WriteLine("Номер строки с наименьшей суммой элементов: " + RealeseMatrix(matrix));
+Console.WriteLine("Начальная матрица: ");
+NewMatrix(matrix);
+SortRowInMatrix(matrix);
