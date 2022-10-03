d6bb947a9ba4fcea34578b9a22c983a8165c8bff
ulyana_belousova <tentakly@list.ru>
2022-10-03 14:30:13 +0300

    task 60

--- task_60/Program.cs
+++ task_60/Program.cs
@@ -1,26 +1,34 @@
 ﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента
-void InputMatrix(int[,,] matrix)
+void NewMatrix(int[,,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
+    {
         for (int j = 0; j < matrix.GetLength(1); j++)
-            for (int k = 0; k < matrix.GetLength(2); k++)
-                matrix[i, j, k] = new Random().Next(-10,11);
+        {
+            for (int  k  = 0; k < matrix.GetLength(2); k++)
+                matrix[i, j, k] = new Random().Next(-10, 11);
+        }
+    }
 }
 
-void PrintMatrix(int[,,] matrix)
+void PrintMatrix(int[,,] MatrixOne)
 {
-    for (int i = 0; i < matrix.GetLength(0); i++)
-       for (int j = 0; j < matrix.GetLength(1); j++)
-            for (int k = 0; k < matrix.GetLength(2); k++)
-                Console.WriteLine(matrix[i, j, k] + " (" + i + ", " + j + ", " + k + ")");
+    for (int i = 0; i < MatrixOne.GetLength(0); i++)
+    {
+        for (int j = 0; j < MatrixOne.GetLength(1); j++)
+        {
+            for (int  k  = 0; k < MatrixOne.GetLength(2); k++)
+                Console.WriteLine(MatrixOne[i, j, k] + " (" + i + ", " + j + ", " + k + ")");
+        }
+    }
 }
 
-Console.Write("Введите x: ");
+Console.Write("Введите X: ");
 int x = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите y: ");
+Console.Write("Введите  Y: ");
 int y = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите z: ");
+Console.Write("Введите  Z: ");
 int z = Convert.ToInt32(Console.ReadLine());
-int[,,] matrix = new int[x, y, z];
-InputMatrix(matrix);
-PrintMatrix(matrix);
+int[,,] MatrixOne = new int[x, y, z];
+NewMatrix(MatrixOne);
+PrintMatrix(MatrixOne);
