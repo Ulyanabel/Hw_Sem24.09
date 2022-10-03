cee19af85e0195652eb3f740f702f580775eef5b
ulyana_belousova <tentakly@list.ru>
2022-10-03 13:18:06 +0300

    task 54

--- task_54/Program.cs
+++ task_54/Program.cs
@@ -1,41 +1,35 @@
 ﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
-void InputMatrix(int[,] matrix)
+void NewMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
-            matrix[i, j] = new Random().Next(0, 11);
-    }
-}
-
-void PrintMatrix(int[,] matrix)
-{
-    Console.WriteLine();
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-            Console.Write("{0,5}", matrix[i, j]);
+        {
+            matrix[i, j] = new Random().Next(-10, 11);
+            Console.Write(matrix[i, j] + " \t");
+        }
         Console.WriteLine();
     }
 }
 
-void ReleaseMatrix(int[,] matrix)
+void SortRowInMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        int minElem = matrix[i, 0];
-        for (int k = 0;k < matrix.GetLength(1) - 1;k++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            for (int j = 0; j < (matrix.GetLength(1)) - 1; j++)
+            for (int k = 0; k < matrix.GetLength(1) - 1;k++)
             {
-                if (matrix[i, j] < matrix[i, j + 1])
+                if (matrix[i, k] < matrix[i,  k + 1])
                 {
-                    minElem = matrix[i, j];
-                    matrix[i, j] = matrix[i, j + 1];
-                    matrix[i, j + 1] = minElem;
+                    int temp = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i, k] = temp;
                 }
             }
+            Console.Write(matrix[i, j] + " \t");
         }
+    Console.WriteLine();
     }
 }
 
@@ -44,10 +38,7 @@ int n = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите количествово столбцов: ");
 int m = Convert.ToInt32(Console.ReadLine());
 int[,] matrix = new int[n, m];
-InputMatrix(matrix);
-Console.WriteLine("Исходный массив: ");
-PrintMatrix(matrix);
-ReleaseMatrix(matrix);
-Console.WriteLine();
-Console.WriteLine("Конечный массив: ");
-PrintMatrix(matrix);
+Console.WriteLine("Начальная матрица: ");
+NewMatrix(matrix);
+Console.WriteLine("Конечная матрица: ");
+SortRowInMatrix(matrix);
